cfba0963228bb6ea64ea725431bf325dd65fa5b1
ValentinMoreau08 <valentin.moreau08@gmail.com>
2020-12-16 14:37:02 +0100

    Add createTime in UserService

--- src/main/java/fr/tse/poc/service/impl/UserServiceImpl.java
+++ src/main/java/fr/tse/poc/service/impl/UserServiceImpl.java
@@ -1,23 +1,28 @@
 package fr.tse.poc.service.impl;
 
+import java.util.Date;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.tse.poc.dao.ManagerRepository;
+import fr.tse.poc.dao.ProjectRepository;
+import fr.tse.poc.dao.TimeRepository;
 import fr.tse.poc.dao.UserRepository;
 import fr.tse.poc.domain.Manager;
+import fr.tse.poc.domain.Project;
+import fr.tse.poc.domain.Time;
 import fr.tse.poc.domain.User;
 import fr.tse.poc.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
-	@Autowired
-	UserRepository userRepository;
 	
-	@Autowired
-	ManagerRepository managerRepository;
+	private @Autowired UserRepository userRepository;
+	private @Autowired ManagerRepository managerRepository;
+	private @Autowired TimeRepository timeRepository;
+	private @Autowired ProjectRepository projectRepository;
 	
 	@Override
 	public User createUser(String login, String password, String name, String firstname) {
@@ -35,6 +40,21 @@ public class UserServiceImpl implements UserService{
 		managerRepository.save(manager);
 		userRepository.save(user);
 		return user;
+	}
+
+	@Override
+	public Time createTime(User user, Project project, int time, Date date) {
+		Time ti = new Time(time, date);
+		ti.setUser(user);
+		ti.setProject(project);
+		
+		user.addTime(ti);
+		project.addTime(ti);
+		
+		ti = timeRepository.save(ti);
+		userRepository.save(user);
+		projectRepository.save(project);
+		return ti;
 	}
 
 }

f690d0e610d278b394ea3fbdfc2b744eddea417a
ValentinMoreau08 <valentin.moreau08@gmail.com>
2020-12-16 15:49:20 +0100

    Add UserController

--- src/main/java/fr/tse/poc/controller/UserController.java
+++ src/main/java/fr/tse/poc/controller/UserController.java
@@ -16,26 +16,23 @@ import fr.tse.poc.domain.Time;
 import fr.tse.poc.domain.User;
 import fr.tse.poc.service.UserService;
 
-//@CrossOrigin car par défaut, il est interdit de faire des requêtes CrossOrigin (ie client qui fait la requête ne vient pas du même serveur qui expose l'api)
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.PATCH})
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+	private @Autowired UserService userService;
 	
 	@GetMapping("/users")
 	public Collection<User> findAllUsers(){
-		return null;
+		return userService.findAllUsers();
 	}
 	
 	@PostMapping("/users/{id}/times")
 	public Time createTime(@RequestBody Time time, @PathVariable Long id) {
 		//User user = this.userService.getUserById(id);
-		//User user = userServi
-		//Project project = time.getProject();
-		//return userService.createTime(user, project, time.getTime(), time.getDate());
-		return null;
+		User user = userService.findAllUsers().iterator().next(); // A changer, c'est juste pour tester
+		Project project = time.getProject();
+		return userService.createTime(user, project, time.getTime(), time.getDate());
 	}
 	
 }

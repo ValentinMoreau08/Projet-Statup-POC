ed9013159097586212480c386268ae590537ac31
bubunzer <sobunung@yahoo.fr>
2020-12-16 13:58:09 +0100

    Update ProjectServiceImpl.java

--- src/main/java/fr/tse/poc/service/impl/ProjectServiceImpl.java
+++ src/main/java/fr/tse/poc/service/impl/ProjectServiceImpl.java
@@ -1,14 +1,15 @@
-package fr.tse.poc.service;
+package fr.tse.poc.service.impl;
 
 import fr.tse.poc.dao.ProjectRepository;
 import fr.tse.poc.domain.Project;
+import fr.tse.poc.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
 @Service
-public class ProjectServiceImpl implements ProjectService{
+public class ProjectServiceImpl implements ProjectService {
 
     @Autowired
     private ProjectRepository projectRepository;

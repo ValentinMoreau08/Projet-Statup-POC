dbe7cef63a2880452774abfabb4b88a1c3ba6e04
lailaab97 <lailaabouzaid@gmail.com>
2021-01-14 21:32:35 +0100

    change method from post to patch

--- src/main/java/fr/tse/poc/controller/UserController.java
+++ src/main/java/fr/tse/poc/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,7 +55,7 @@ public class UserController {
 	
 
 	
-	@PostMapping("/users/{id}")
+	@PatchMapping("/users/{id}")
 	public User changeManagerOfUser(User user, User manager) {
 		return this.changeManagerOfUser(user, manager);
 	}

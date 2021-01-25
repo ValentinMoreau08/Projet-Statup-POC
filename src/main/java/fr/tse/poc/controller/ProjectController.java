06d3d9a1ca39fb8b52869b773b387dad015510f2
bubunzer <sobunung@yahoo.fr>
2021-01-25 11:38:16 +0100

    #8 add Project

---
+++ src/main/java/fr/tse/poc/controller/ProjectController.java
@@ -0,0 +1,38 @@
+package fr.tse.poc.controller;
+
+import java.util.Collection;
+
+import fr.tse.poc.domain.Project;
+import fr.tse.poc.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.tse.poc.domain.Time;
+import fr.tse.poc.domain.User;
+import fr.tse.poc.service.UserService;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.PATCH})
+public class ProjectController {
+
+    private @Autowired ProjectService projectService;
+
+    @GetMapping("/projects")
+    public Collection<Project> findAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    // On va plus utiliser un dto (data transfert object) pour éviter de mettre in Time, et faire passer cet objet directement dans la couche service comme vu en cours
+    // Car le contrôler n'a pas d'intelligence il ne sert qu'à faire le lien !
+
+    @PostMapping("/projects")
+    public Project createProject(@RequestBody String name, String clientName, String description){
+        return this.projectService.createProject(name, clientName, description);
+    }
+}
